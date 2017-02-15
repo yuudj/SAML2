a290f77f390a2618abbad57dab28aa21f2cbcad1
Anthony Langsworth <anthony.langsworth@gmail.com>
2017-02-15 17:13:03 +1100

    Remove About, Contact. Dump claims on Index.

---
+++ src/TestSSO/Models/IndexModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+
+namespace TestSSO.Models
+{
+    public class IndexModel
+    {
+        public ClaimsIdentity ClaimsIdentity { get; set; }
+    }
+}
